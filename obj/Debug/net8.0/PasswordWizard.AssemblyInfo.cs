a7512656b7325b329f935c8ff7ef8660017ca54d
Funky <82616867+funkychicken493@users.noreply.github.com>
2024-06-10 15:43:21 -0500

    grah

--- obj/Debug/net8.0/PasswordWizard.AssemblyInfo.cs
+++ obj/Debug/net8.0/PasswordWizard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordWizard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7aaf77340891957b1d88123d74d863be323fc3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordWizard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordWizard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
